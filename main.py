cbcb2e4ed0417dfe1886c6318b491d2921fc8cb9
James-Wukong <qianfan.cheng.cn@gmail.com>
2023-12-21 23:25:52 -0500

    raw idea
    create the basic structure of project, and also the idea.

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
+import toml
+import os
+from datetime import date, datetime, timedelta
+from src.constants import ROOT_DIR
+from src.database.sqlconn import DbFactory, MySqlDB, PostgreSqlDB
+
+config = toml.load(os.path.join(ROOT_DIR, 'conf', 'config.toml'))
 
 if __name__ == '__main__':
-    pass
+    mysql_param = {
+        'host': config['database']['mysql']['host'],
+        'user': config['database']['mysql']['user'],
+        'password': config['database']['mysql']['password'],
+        'database': config['database']['mysql']['db_name']
+    }
+
+    postgres_param = {
+        'host': config['database']['postgres']['host'],
+        'user': config['database']['postgres']['user'],
+        'password': config['database']['postgres']['password'],
+        'database': config['database']['postgres']['db_name']
+    }
+
+    
