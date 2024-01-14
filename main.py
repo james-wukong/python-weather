817ddfcf659a1efbbcff19dc2851e20588c0bd3b
James Cheng <qianfan.cheng.cn@gmail.com>
2024-01-14 00:32:50 -0500

    update weather api

--- main.py
+++ main.py
@@ -7,6 +7,6 @@ from src.database.mysql_crud import insert_weather_data
 config = toml.load(os.path.join(ROOT_DIR, 'conf', 'config.toml'))
 
 if __name__ == '__main__':
-    a = Weather()
-    _, weather_insert_data = a.fetch_weather_data()
-    a.mysql_weather_insert_data(weather_insert_data)
+    a = Weather(loc='Missisauga,Ontario,CA', date_start='2023-11-14')
+    # _, weather_insert_data = a.fetch_weather_data()
+    a.mysql_weather_insert_data()
